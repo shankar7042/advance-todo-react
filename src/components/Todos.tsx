e1ede107270da2770c22d3810463cc986a7c3ba8
shankar7042 <shankar.kumar.4171@gmail.com>
2024-08-24 03:17:13 +0530

    correct bug of filtered todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,14 @@
-import { selectTodos } from "../feature/todos/todoSlice";
+import { selectFilteredTodos } from "../feature/todos/todoSlice";
 import { useAppSelector } from "../redux/hooks";
 import Todo from "./Todo";
 
 const Todos = () => {
-  const todos = useAppSelector(selectTodos);
+  const filteredTodos = useAppSelector(selectFilteredTodos);
 
   return (
     <div className="mb-2">
       <ul>
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <Todo todo={todo} key={todo.id} />
         ))}
       </ul>
