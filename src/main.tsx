7ee1667cc1de6e410e4481a52342b464632474da
shankar7042 <shankar.kumar.4171@gmail.com>
2024-08-24 03:14:32 +0530

    removed context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { TodosProvider } from "./context-provider/TodosProvider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <TodosProvider>
-        <App />
-      </TodosProvider>
+      <App />
     </Provider>
   </StrictMode>
 );
